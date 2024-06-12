a420275dcdcd1d76685b069728b6571434ecb08b
Megan <megan.marson22@gmail.com>
2024-06-12 13:41:48 -0400

    Update to db.py and main.py to add Depends

--- db.py
+++ db.py
@@ -1,13 +1,15 @@
+from sqlmodel import create_engine, Session
+
 DATABASE_URL = 'postgresql://postgres@localhost/crud_u'
 
-from sqlmodel import create_engine, SQLModel, Session
+# from sqlmodel import create_engine, SQLModel, Session
 
 engine = create_engine(DATABASE_URL, echo=True)
 
-def init_db():
-    SQLModel.metadata.create_all(engine)
+# def init_db():
+#     SQLModel.metadata.create_all(engine)
     
-    def get_session():
+def get_session():
         with Session(engine) as session:
             yield session
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware 
 from sqlmodel import Session, select, func
-from db import engine 
+from db import get_session
 from models.courses import Courses
 from models.enrollments import Enrollments
 from models.students import Students
@@ -27,33 +27,31 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/students")
-def list_students():
-    with Session(engine) as session:
+def list_students(session: Session = Depends(get_session)):
+    # with Session(engine) as session:
         statement = select(Students)
         results = session.exec(statement).all()
-    return results
+        return results
 
 @app.get("/courses")
-def list_courses():
-    with Session(engine) as session:
+def list_courses(session: Session = Depends(get_session)):
+    # with Session(engine) as session:
         statement = select(Courses)
         results = session.exec(statement).all()
-    return results
+        return results
         
 @app.get("/enrollments")
-def list_enrollments():
-    with Session(engine) as session:
-    # SELECT students.name, courses.name FROM Enrollments
-    # INNER JOIN students ON students.id = enrollments.student_id
-    # INNER JOIN courses ON course.id = enrollments.course_id;
-    
-        statement = select(Courses.name.label('course_name'), 
+def list_enrollments(session: Session = Depends(get_session)):
+    statement = select(Courses.name.label('course_name'), 
                            func.array_agg(Students.name).label('students')
                     ).select_from (Enrollments).join(Students,Students.id == Enrollments.student_id).join(Courses, Courses.id == Enrollments.course_id).group_by(Courses.name)
     results = session.exec(statement).mappings().all()
     return results
    
-
+ # SELECT students.name, courses.name FROM Enrollments
+    # INNER JOIN students ON students.id = enrollments.student_id
+    # INNER JOIN courses ON course.id = enrollments.course_id;
+    
 # @app.get("/")
 
 if __name__== '__main__':
